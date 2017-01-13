3c8dcb8ce65479874c88d9d57d053eb48cb3b42e
Lagz0ne <duc@outlook.sg>
2017-01-13 15:56:19 +0700

    Minor change

--- server.js
+++ server.js
@@ -19,16 +19,24 @@ const server = http.createServer((req, res) => {
     }
 
     const {outcome, vcs_revision, build_num} = json;
-    
+
     res.statusCode = 400;
     if (!outcome || outcome === '') return res.end("Invalid status");
     if (outcome !== 'success') return res.end("Ignored build status");
     if (!vcs_revision || vcs_revision === '') return res.end("Invalid revision hash");
     if (!build_num || build_num === '') return res.end("Invalid build number");
 
+    const delegate = () => {
+      echo(JSON.stringify(json)).exec(CIRCLECI_WEBHOOK_COMMAND, {async:true}, (code, stdout, stderr) => {
+        console.log('Exit code:', code);
+        console.log('Program output:', stdout);
+        console.log('Program stderr:', stderr);
+      });
+    };
+
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({status: `Acknowledged circle ci build ${build_num} of revision ${vcs_revision}`}));
-    delegate(json);
+    setTimeout(delegate);
+    return res.end(JSON.stringify({status: `Acknowledged circle ci build ${build_num} of revision ${vcs_revision}`}));
   });
 });
 
@@ -36,7 +44,3 @@ server.on('clientError', (err, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
 server.listen(CIRCLECI_WEBHOOK_PORT);
-
-const delegate = (json) => {
-  echo(JSON.stringify(json)).exec(CIRCLECI_WEBHOOK_COMMAND, {async:true});
-}
